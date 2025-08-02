4122342f59ae7dbc2a57afbfd3b7174965b487dc
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-08-02 16:00:11 +0300

    Binding

--- OOP05/Program.cs
+++ OOP05/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace OOP05
+﻿using OOP05.Binding;
+
+namespace OOP05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Binding 
+           // TypeA type= new TypeB(1, 2);
+           // type.A = 10;
+           //// type.B= 20;
+           // type.Fun01(); // Calls TypeA's Fun01 due to 'new' keyword in TypeB
+           // type.Fun02(); // Calls TypeB's Fun02 due to 'override' keyword in TypeB
+            #endregion
         }
     }
 }
