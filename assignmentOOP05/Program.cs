7d45edc9e07e8240729485485f5f62be4e5f8683
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-08-02 19:03:03 +0300

    Q1

--- assignmentOOP05/Program.cs
+++ assignmentOOP05/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace assignmentOOP05
+﻿using assignmentOOP05.Q01;
+
+namespace assignmentOOP05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+      
+            IShape circle = new Circle(5);
+            circle.DisplayShapeInfo();
+
+            IShape rectangle = new Rectangle(4, 6);
+            rectangle.DisplayShapeInfo();
+           
+            #endregion
         }
     }
 }
