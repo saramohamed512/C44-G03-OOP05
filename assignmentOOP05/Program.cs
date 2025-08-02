be315ae5448f4e0b781f40559b0717a2b0d6f197
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-08-02 19:18:50 +0300

    Q02

--- assignmentOOP05/Program.cs
+++ assignmentOOP05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using assignmentOOP05.Q01;
+using assignmentOOP05.Q02;
 
 namespace assignmentOOP05
 {
@@ -7,14 +8,24 @@ namespace assignmentOOP05
         static void Main(string[] args)
         {
             #region Q01
-      
-            IShape circle = new Circle(5);
-            circle.DisplayShapeInfo();
 
-            IShape rectangle = new Rectangle(4, 6);
-            rectangle.DisplayShapeInfo();
-           
+            //IShape circle = new Circle(5);
+            //circle.DisplayShapeInfo();
+
+            //IShape rectangle = new Rectangle(4, 6);
+            //rectangle.DisplayShapeInfo();
+
             #endregion
+            #region Q02
+            //IAuthenticationService authService = new BasicAuthenticationService();
+            //bool isAuthenticated = authService.AuthenticateUser("admin", "password123");
+            //bool isAuthorized = authService.AuthorizeUser("admin", "Admin");
+
+            //Console.WriteLine($"Authentication: {isAuthenticated}");
+            //Console.WriteLine($"Authorization: {isAuthorized}");
+    
+            #endregion
+
         }
     }
 }
